b5e3107c9d6160582ff37bab936fe4cb0743a239
Smilingvk <valeriaguzmanavalos@gmail.com>
2026-02-04 14:25:10 -0500

    categories added

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,5 @@
 import { loadHeaderFooter } from './utils.mjs';
 
-import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
-
+// Only load header and footer on the home page
+// Products will be shown on the product listing page
 loadHeaderFooter();
-
-const dataSource = new ProductData("tents");
-const listElement = document.querySelector(".product-list");
-
-const productList = new ProductList("tents", dataSource, listElement);
-productList.init();
